208f687c3d9fa28bca2bc867f266d5ceb8d65444
primer push <lautarodisalvo2023@gmail.com>
2023-09-06 00:39:52 -0300

    primer push

---
+++ code/archivoBase.js
@@ -0,0 +1,13 @@
+export default function HamMenu(hamButton, insideBtn1 ,insideBtn2, menuOptions){
+    let d = document
+    
+    d.addEventListener('click', (e)=>{
+        if(e.target.matches(hamButton)
+            || e.target.matches(insideBtn1)
+            || e.target.matches(insideBtn2)){
+            d.querySelector(menuOptions)
+            .classList.toggle('on');
+        }
+    })
+    
+    }
---
+++ code/ham.js
@@ -0,0 +1,13 @@
+import HamMenu from "./archivoBase.js"
+
+
+
+document.addEventListener('DOMContentLoaded', e=>{
+    
+    HamMenu('.hamburger',
+    '.hamburger-box  ',
+    ' .hamburger-inner ',
+    '.group-options-menuMobile')
+})
+
+
