688fb752849df60e1f5b85e85a425a4bdf744382
primer push <lautarodisalvo2023@gmail.com>
2023-09-06 02:44:39 -0300

    nuevas cosas

--- code/archivoBase.js
+++ code/archivoBase.js
@@ -2,11 +2,9 @@ export default function HamMenu(hamButton, insideBtn1 ,insideBtn2, menuOptions){
     let d = document
     
     d.addEventListener('click', (e)=>{
-        if(e.target.matches(hamButton)
-            || e.target.matches(insideBtn1)
-            || e.target.matches(insideBtn2)){
-            d.querySelector(menuOptions)
-            .classList.toggle('on');
+        if(e.target.matches(hamButton)|| e.target.matches(insideBtn1)|| e.target.matches(insideBtn2)){
+            d.querySelector(menuOptions).classList.toggle('on');
+            d.querySelector(hamButton).classList.toggle('is-active');
         }
     })
     
